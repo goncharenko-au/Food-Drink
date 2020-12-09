3e0e585bd232109d5bb73f13ceea023c81ba24cd
Alina <goncharenko0206@gmail.com>
2020-12-09 22:15:23 +0200

    added new-wines block

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+let mySwiper = new Swiper(".new-wines__slider", {
+    // Optional parameters
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Slider
+    let mySwiper = new Swiper(".new-wines__slider", {
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+
+
+
+
+
+});
