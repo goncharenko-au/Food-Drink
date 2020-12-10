05e6588bd730304cff15baa7e36207d63b210fe3
Alina <goncharenko0206@gmail.com>
2020-12-10 22:53:12 +0200

    added future-products block

--- dist/js/script.js
+++ dist/js/script.js
@@ -23,4 +23,45 @@ document.addEventListener("DOMContentLoaded", () => {
         learnBtn.classList.add('active');
     });
 
+
+    // tabs
+    const tabs = document.querySelectorAll('.future-products__type'),
+        tabsContent = document.querySelectorAll('.future-products__catalog'),
+        tabsParent = document.querySelector('.future-products__types');
+
+
+    function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.style.display = "none";
+        });
+        tabs.forEach(item => {
+            item.classList.remove('future-products__catalog_active');
+            item.style.cssText = `color: #fff; border-bottom: none`;
+        });
+    }
+
+    function showTabsContent(i = 0) {
+        tabsContent[i].style.display = 'flex';
+        tabs[i].classList.add('future-products__catalog_active');
+        tabs[i].style.cssText = `color: #e2b024; border-bottom-color: #e2b024; z-index: 2`;
+
+    }
+    hideTabsContent();
+    showTabsContent();
+
+    tabsParent.addEventListener('click', () => {
+        if (event.target && event.target.classList.contains('future-products__type')) {
+
+            tabs.forEach((item, index) => {
+                if (event.target == item) {
+                    hideTabsContent();
+                    showTabsContent(index);
+                }
+            });
+        }
+
+    });
+
+
+
 });
